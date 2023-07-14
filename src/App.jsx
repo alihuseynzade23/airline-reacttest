8e8b9ff93d757149082e792329aa3a1a9e03e6a0
Ali <86948893+AleksHNZ@users.noreply.github.com>
2023-07-14 14:25:06 +0400

    End of the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./layout/Header";
+import { Footer } from "./layout/Footer";
+import { MainSection } from "./layout/MainSection";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <MainSection />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ src/layout/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav className='#43a047 green darken-1'>
+    <div classNameName="nav-wrapper">
+      <a href="#" className="brand-logo">Airline API</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="#">Repo</a></li>
+      </ul>
+    </div>
+  </nav>
+    )
+}
+
+export {Header}
